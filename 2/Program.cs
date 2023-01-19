56e1c5314aa945cc0746a4c8511618717f88580d
Artem <proceller@yandex.ru>
2023-01-19 20:15:01 +0300

    idea but not working

---
+++ 2/Program.cs
@@ -0,0 +1,34 @@
+﻿int InputInt(string message)
+{
+    Console.Write(message + ":");
+    string? inputValue = Console.ReadLine();
+    int result = Convert.ToInt32(inputValue);
+    return result;
+}
+
+int Check(int num)
+{
+    if (num < 99)
+    {
+        System.Console.WriteLine("в числе нет третьей цифры");
+    }
+    if (num > 1000)
+    {
+        while (num > 1000)
+        {
+            num = num / 10;
+        }
+    }
+return num;
+}
+
+int num = InputInt("введите число");
+Check(num);
+num = num % 100;
+num = num % 10;
+System.Console.WriteLine($"третья цифра введенного числа это {num}");
+
+
+
+
+
